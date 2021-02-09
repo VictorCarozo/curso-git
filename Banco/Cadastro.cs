192fcb6b747565b950883d19d2a140b35b807b83
Victor <vcarozo@gmail.com>
2021-02-09 11:44:26 -0300

    Projeto cadastro de banco

---
+++ Banco/Cadastro.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+
+namespace Banco
+{
+    class Cadastro
+    {
+        public int Conta { get; private set; }
+        public string Titular { get; set; }
+        public double Saldo { get; private set; }
+
+        public Cadastro(int conta, string titular)
+        {
+            Conta = conta;
+            Titular = titular;
+            Saldo = 0.0;
+        }
+
+        public Cadastro(int conta, string titular, double saldo) : this(conta,titular)
+        {
+            Saldo = saldo;
+        }
+
+        public override string ToString()
+        {
+            return "Conta : " 
+            + Conta
+            + ", Titular : "
+            + Titular
+            + ", Saldo: $ "
+            + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+        public void Deposito(double saldo)
+        {
+            Saldo += saldo;
+        }
+        public void Saque(double saldo)
+        {
+            
+            Saldo -= saldo + 5;
+        }
+        
+    }
+}
+
---
+++ Banco/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Globalization;
+
+namespace Banco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                Cadastro c;
+                Console.Write("Entre com o número da conta: ");
+                int conta = int.Parse(Console.ReadLine());
+
+                Console.Write("Entre com o titular da conta: ");
+                string titular =Console.ReadLine();
+
+                Console.Write("Hávera deposito inicial (s/n)? ");
+                char depini = char.Parse( Console.ReadLine());
+
+                if (depini == 's') {
+                  Console.Write("Entre com valor de depósito inicial: ");
+                  double depositoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                  c = new Cadastro(conta, titular, depositoInicial);
+                 }
+                else {
+                  c = new Cadastro(conta, titular);
+                }
+               
+                Console.WriteLine();
+                Console.WriteLine("Dados da conta: " + c);
+                
+                Console.WriteLine();
+                Console.Write("Entre com um valor para déposito: ");
+                double qte = double.Parse(Console.ReadLine());
+                c.Deposito(qte);
+                
+                Console.WriteLine();
+                Console.WriteLine("Dados da conta atualizados: " + c);
+                
+                Console.WriteLine();
+                Console.Write("Entre com um valor para saque: ");
+                qte = double.Parse(Console.ReadLine());
+                c.Saque(qte);
+                
+                Console.WriteLine();
+                Console.WriteLine("Dados atualizados: " + c);
+               
+            
+        }
+    }
+}
